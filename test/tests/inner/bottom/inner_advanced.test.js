f59478c178fb08ac129f8283d7ce9c9a9f23bbd4
thislooksfun <thislooksfun@repbot.org>
2020-02-23 11:55:20 -0600

    Update tests for Node 12+
    In Node 12 (or possibly 11) the format of the error thrown by 'require' changed, which broke the tests.

--- test/tests/inner/bottom/inner_advanced.test.js
+++ test/tests/inner/bottom/inner_advanced.test.js
@@ -16,10 +16,10 @@ describe("Inner (advanced)", function() {
       expect(pquire("baz")).to.equal("inner/baz");
     });
     it("should throw if nothing found", function() {
-      expect(function() { pquire("invalid"); }).to.throw(/^Cannot find module '\.\/\.\.\/invalid'$/);
+      expect(function() { pquire("invalid"); }).to.throw(/^Cannot find module '\.\/\.\.\/invalid'/);
     });
     it("should properly error on nested requires", function() {
-      expect(function() { pquire("../nested"); }).to.throw(/^Cannot find module '\.\/invalid'$/);
+      expect(function() { pquire("../nested"); }).to.throw(/^Cannot find module '\.\/invalid'/);
     });
     
     describe("pquire.withBaseRelative.rel", function() {
@@ -30,13 +30,13 @@ describe("Inner (advanced)", function() {
         expect(pquire.rel("foo")).to.equal("inner/bottom/foo");
       });
       it("should not require absolutely", function() {
-        expect(function() { pquire.rel("baz"); }).to.throw(/^Cannot find module '\.\/baz'$/);
+        expect(function() { pquire.rel("baz"); }).to.throw(/^Cannot find module '\.\/baz'/);
       });
       it("should throw if nothing found", function() {
-        expect(function() { pquire.rel("invalid"); }).to.throw(/^Cannot find module '\.\/invalid'$/);
+        expect(function() { pquire.rel("invalid"); }).to.throw(/^Cannot find module '\.\/invalid'/);
       });
       it("should properly error on nested requires", function() {
-        expect(function() { pquire.rel("../../nested"); }).to.throw(/^Cannot find module '\.\/invalid'$/);
+        expect(function() { pquire.rel("../../nested"); }).to.throw(/^Cannot find module '\.\/invalid'/);
       });
     });
 
@@ -45,19 +45,19 @@ describe("Inner (advanced)", function() {
         expect(pquire.abs).to.be.a("function");
       });
       it("should not require relatively", function() {
-        expect(function() { pquire.abs("btm"); }).to.throw(/^Cannot find module '\.\/\.\.\/btm'$/);
+        expect(function() { pquire.abs("btm"); }).to.throw(/^Cannot find module '\.\/\.\.\/btm'/);
       });
       it("should not require relative to project root", function() {
-        expect(function() { pquire.abs("bar"); }).to.throw(/^Cannot find module '\.\/\.\.\/bar'$/);
+        expect(function() { pquire.abs("bar"); }).to.throw(/^Cannot find module '\.\/\.\.\/bar'/);
       });
       it("should require absolutely relative to given path", function() {
         expect(pquire.abs("foo")).to.equal("inner/foo");
       });
       it("should throw if nothing found", function() {
-        expect(function() { pquire.abs("invalid"); }).to.throw(/^Cannot find module '\.\/\.\.\/invalid'$/);
+        expect(function() { pquire.abs("invalid"); }).to.throw(/^Cannot find module '\.\/\.\.\/invalid'/);
       });
       it("should properly error on nested requires", function() {
-        expect(function() { pquire.abs("../nested"); }).to.throw(/^Cannot find module '\.\/invalid'$/);
+        expect(function() { pquire.abs("../nested"); }).to.throw(/^Cannot find module '\.\/invalid'/);
       });
     });
     
--- test/tests/inner/inner.test.js
+++ test/tests/inner/inner.test.js
@@ -15,10 +15,10 @@ describe("Inner", function() {
       expect(pquire("bar")).to.equal("bar");
     });
     it("should throw if nothing found", function() {
-      expect(function() { pquire("invalid"); }).to.throw(/^Cannot find module '\.\/\.\.\/invalid'$/);
+      expect(function() { pquire("invalid"); }).to.throw(/^Cannot find module '\.\/\.\.\/invalid'/);
     });
     it("should properly error on nested requires", function() {
-      expect(function() { pquire("nested"); }).to.throw(/^Cannot find module '\.\/invalid'$/);
+      expect(function() { pquire("nested"); }).to.throw(/^Cannot find module '\.\/invalid'/);
     });
   });
   
@@ -30,13 +30,13 @@ describe("Inner", function() {
       expect(pquire.rel("foo")).to.equal("inner/foo");
     });
     it("should not require absolutely", function() {
-      expect(function() { pquire.rel("bar"); }).to.throw(/^Cannot find module '\.\/bar'$/);
+      expect(function() { pquire.rel("bar"); }).to.throw(/^Cannot find module '\.\/bar'/);
     });
     it("should throw if nothing found", function() {
-      expect(function() { pquire.rel("invalid"); }).to.throw(/^Cannot find module '\.\/invalid'$/);
+      expect(function() { pquire.rel("invalid"); }).to.throw(/^Cannot find module '\.\/invalid'/);
     });
     it("should properly error on nested requires", function() {
-      expect(function() { pquire.rel("../nested"); }).to.throw(/^Cannot find module '\.\/invalid'$/);
+      expect(function() { pquire.rel("../nested"); }).to.throw(/^Cannot find module '\.\/invalid'/);
     });
   });
   
@@ -45,16 +45,16 @@ describe("Inner", function() {
       expect(pquire.abs).to.be.a("function");
     });
     it("should not require relatively", function() {
-      expect(function() { pquire.abs("baz"); }).to.throw(/^Cannot find module '\.\/\.\.\/baz'$/);
+      expect(function() { pquire.abs("baz"); }).to.throw(/^Cannot find module '\.\/\.\.\/baz'/);
     });
     it("should require absolutely", function() {
       expect(pquire.abs("foo")).to.equal("foo");
     });
     it("should throw if nothing found", function() {
-      expect(function() { pquire.abs("invalid"); }).to.throw(/^Cannot find module '\.\/\.\.\/invalid'$/);
+      expect(function() { pquire.abs("invalid"); }).to.throw(/^Cannot find module '\.\/\.\.\/invalid'/);
     });
     it("should properly error on nested requires", function() {
-      expect(function() { pquire.abs("nested"); }).to.throw(/^Cannot find module '\.\/invalid'$/);
+      expect(function() { pquire.abs("nested"); }).to.throw(/^Cannot find module '\.\/invalid'/);
     });
   });
 });
